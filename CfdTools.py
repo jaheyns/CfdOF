8cba45270d88a5bd6e8db80b4946a8f38ed49d30
Oliver Oxtoby <oliveroxtoby@gmail.com>
2021-02-24 10:23:32 +0200

    Do not normalise OpenFOAM path before storing, to allow for empty path

--- CfdTools.py
+++ CfdTools.py
@@ -366,7 +366,7 @@ def getPreferencesLocation():
 def setFoamDir(installation_path):
     prefs = getPreferencesLocation()
     # Set OpenFOAM install path in parameters
-    FreeCAD.ParamGet(prefs).SetString("InstallationPath", os.path.normpath(installation_path))
+    FreeCAD.ParamGet(prefs).SetString("InstallationPath", installation_path)
 
 
 def getFoamDir():
@@ -380,7 +380,7 @@ def getFoamDir():
     if not installation_path:
         installation_path = detectFoamDir()
 
-    return installation_path
+    return os.path.normpath(installation_path)
 
 
 def getFoamRuntime():
