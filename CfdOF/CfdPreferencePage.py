47a292aa9ffe98084595369f928e9aa6242da69b
Jonathan Bergh <icojb25@users.noreply.github.com>
2024-03-29 08:39:03 +0100

    Update copyright

--- CfdOF/CfdPreferencePage.py
+++ CfdOF/CfdPreferencePage.py
@@ -4,6 +4,7 @@
 # *   Copyright (c) 2017 Johan Heyns (CSIR) <jheyns@csir.co.za>             *
 # *   Copyright (c) 2017 Alfred Bogaers (CSIR) <abogaers@csir.co.za>        *
 # *   Copyright (c) 2019-2023 Oliver Oxtoby <oliveroxtoby@gmail.com>        *
+# *   Copytight (c) 2024 Jonathan Bergh <bergh.jonathan@gmail.com>          *
 # *                                                                         *
 # *   This program is free software: you can redistribute it and/or modify  *
 # *   it under the terms of the GNU Lesser General Public License as        *
