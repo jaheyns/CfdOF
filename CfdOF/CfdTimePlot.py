fee3821336403ef440d1d03f1bd4cba13346cb9f
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2025-11-25 14:07:20 -0500

    Update copyright headers

--- CfdOF/CfdTimePlot.py
+++ CfdOF/CfdTimePlot.py
@@ -1,28 +1,29 @@
 # SPDX-License-Identifier: LGPL-3.0-or-later
 # SPDX-FileNotice: Part of the CfdOF addon.
 
-# ***************************************************************************
-# *                                                                         *
-# *   Copyright (c) 2017 Oliver Oxtoby (CSIR) <ooxtoby@csir.co.za>          *
-# *   Copyright (c) 2017 Johan Heyns (CSIR) <jheyns@csir.co.za>             *
-# *   Copyright (c) 2017 Alfred Bogaers (CSIR) <abogaers@csir.co.za>        *
-# *   Copyright (c) 2019-2022 Oliver Oxtoby <oliveroxtoby@gmail.com>        *
-# *                                                                         *
-# *   This program is free software: you can redistribute it and/or modify  *
-# *   it under the terms of the GNU Lesser General Public License as        *
-# *   published by the Free Software Foundation, either version 3 of the    *
-# *   License, or (at your option) any later version.                       *
-# *                                                                         *
-# *   This program is distributed in the hope that it will be useful,       *
-# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
-# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
-# *   GNU Lesser General Public License for more details.                   *
-# *                                                                         *
-# *   You should have received a copy of the GNU Lesser General Public      *
-# *   License along with this program.  If not,                             *
-# *   see <https://www.gnu.org/licenses/>.                                  *
-# *                                                                         *
-# ***************************************************************************
+################################################################################
+#                                                                              #
+#   Copyright (c) 2017 Oliver Oxtoby (CSIR) <ooxtoby@csir.co.za>               #
+#   Copyright (c) 2017 Johan Heyns (CSIR) <jheyns@csir.co.za>                  #
+#   Copyright (c) 2017 Alfred Bogaers (CSIR) <abogaers@csir.co.za>             #
+#   Copyright (c) 2019-2022 Oliver Oxtoby <oliveroxtoby@gmail.com>             #
+#                                                                              #
+#   This program is free software; you can redistribute it and/or              #
+#   modify it under the terms of the GNU Lesser General Public                 #
+#   License as published by the Free Software Foundation; either               #
+#   version 3 of the License, or (at your option) any later version.           #
+#                                                                              #
+#   This program is distributed in the hope that it will be useful,            #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of             #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                       #
+#                                                                              #
+#   See the GNU Lesser General Public License for more details.                #
+#                                                                              #
+#   You should have received a copy of the GNU Lesser General Public License   #
+#   along with this program; if not, write to the Free Software Foundation,    #
+#   Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.        #
+#                                                                              #
+################################################################################
 
 import sys
 import math
--- InitGui.py
+++ InitGui.py
@@ -1,28 +1,30 @@
 # SPDX-License-Identifier: LGPL-3.0-or-later
 # SPDX-FileNotice: Part of the CfdOF addon.
 
-# ***************************************************************************
-# *   (c) bernd hahnebach (bernd@bimstatik.org) 2014                        *
-# *   (c) qingfeng xia @ iesensor.com 2016                                  *
-# *   Copyright (c) 2017 Andrew Gill (CSIR) <agill@csir.co.za>              *
-# *   Copyright (c) 2019-2024 Oliver Oxtoby <oliveroxtoby@gmail.com>        *
-# *   Copyright (c) 2022 Jonathan Bergh <bergh.jonathan@gmail.com>          *
-# *                                                                         *
-# *   This program is free software: you can redistribute it and/or modify  *
-# *   it under the terms of the GNU Lesser General Public License as        *
-# *   published by the Free Software Foundation, either version 3 of the    *
-# *   License, or (at your option) any later version.                       *
-# *                                                                         *
-# *   This program is distributed in the hope that it will be useful,       *
-# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
-# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
-# *   GNU Lesser General Public License for more details.                   *
-# *                                                                         *
-# *   You should have received a copy of the GNU Lesser General Public      *
-# *   License along with this program.  If not,                             *
-# *   see <https://www.gnu.org/licenses/>.                                  *
-# *                                                                         *
-# **************************************************************************/
+################################################################################
+#                                                                              #
+#   (c) bernd hahnebach (bernd@bimstatik.org) 2014                        *
+#   (c) qingfeng xia @ iesensor.com 2016                                  *
+#   Copyright (c) 2017 Andrew Gill (CSIR) <agill@csir.co.za>              *
+#   Copyright (c) 2019-2024 Oliver Oxtoby <oliveroxtoby@gmail.com>        *
+#   Copyright (c) 2022 Jonathan Bergh <bergh.jonathan@gmail.com>          *
+#                                                                              #
+#   This program is free software; you can redistribute it and/or              #
+#   modify it under the terms of the GNU Lesser General Public                 #
+#   License as published by the Free Software Foundation; either               #
+#   version 3 of the License, or (at your option) any later version.           #
+#                                                                              #
+#   This program is distributed in the hope that it will be useful,            #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of             #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                       #
+#                                                                              #
+#   See the GNU Lesser General Public License for more details.                #
+#                                                                              #
+#   You should have received a copy of the GNU Lesser General Public License   #
+#   along with this program; if not, write to the Free Software Foundation,    #
+#   Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.        #
+#                                                                              #
+################################################################################
 
 import sys
 
