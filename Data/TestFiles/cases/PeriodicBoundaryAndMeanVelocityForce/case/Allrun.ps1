5a237b29c1094476d8efcfcac06519e64faecb73
Kavin Teenakul <andythe_great@protonmail.com>
2026-03-14 21:40:06 +0100

    add screenshot

---
+++ Data/TestFiles/cases/PeriodicBoundaryAndMeanVelocityForce/case/Allrun.ps1
@@ -0,0 +1,80 @@
+function runCommand([string]$cmd)
+{
+    $sol = (Split-Path -Leaf $cmd)
+    & $cmd $args 2>&1 | tee log.$sol
+    $err = $LASTEXITCODE
+    if( ! $LASTEXITCODE -eq 0 )
+    {
+        exit $err
+    }
+}
+
+function runParallel([int]$NumProcs, [string]$cmd)
+{
+    $sol = (Split-Path -Leaf $cmd)
+    & mpiexec -affinity -affinity_layout spr:P:L -np $NumProcs $cmd -parallel $args 2>&1 | tee log.$sol
+    $err = $LASTEXITCODE
+    if( ! $LASTEXITCODE -eq 0 )
+    {
+        exit $err
+    }
+}
+
+# Set piping to file to ascii
+$PSDefaultParameterValues['Out-File:Encoding'] = 'ascii'
+
+# Less verbose error reporting
+$ErrorView = 'CategoryView'
+
+# Copy mesh from mesh case dir if available
+$MESHDIR = "../meshCase"
+if( Test-Path -PathType Leaf $MESHDIR/constant/polyMesh/faces )
+{
+    rm -ErrorAction SilentlyContinue -Recurse -Force constant/polyMesh
+    cp -Recurse $MESHDIR/constant/polyMesh constant/polyMesh
+}
+elseif( !(Test-Path -PathType Leaf constant/polyMesh/faces) )
+{
+    Write-Error "Fatal error: Unable to find mesh in directory $MESHDIR"
+    exit 1
+}
+
+# Set turbulence lib
+if ( $Env:WM_PROJECT_VERSION[0] -eq "v" -or 10 -gt $Env:WM_PROJECT_VERSION )
+{
+    echo '"libturbulenceModels"' > system/turbulenceLib
+}
+else
+{
+    echo '"libmomentumTransportModels"' > system/turbulenceLib
+}
+
+# Set interface compression
+echo "div(phi,alpha) Gauss vanLeer;" > system/alphaDivScheme
+echo "cAlpha 1;" > system/cAlpha
+
+# Update patch name and type
+runCommand createPatch -overwrite
+
+# Parallel decomposition
+if( !(Test-Path -PathType Container processor0) )
+{
+    runCommand decomposePar -force
+}
+
+# Pick up number of parallel processes
+$NPROC = foamDictionary -entry "numberOfSubdomains" -value system/decomposeParDict
+
+# Mesh renumbering
+runParallel $NPROC renumberMesh -overwrite
+
+# Run application in parallel
+# Detect new foamRun in Foundation versions >= 11 and translate solver
+if( (Get-Command -ErrorAction SilentlyContinue foamRun) )
+{
+    runParallel $NPROC foamRun -solver incompressibleFluid
+}
+else
+{
+    runParallel $NPROC simpleFoam
+}
