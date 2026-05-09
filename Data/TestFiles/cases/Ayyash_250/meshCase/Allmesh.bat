a46081a4b599332ca55dd5630e8696a144e47dd8
muezabdalla <muezabdalla777@gmail.com>
2026-05-09 10:20:47 +0300

    add the test data

---
+++ Data/TestFiles/cases/Ayyash_250/meshCase/Allmesh.bat
@@ -0,0 +1,31 @@
+@echo off
+
+REM Source runtime environment
+
+set FOAMDIR="/usr/lib/openfoam/openfoam2512"
+set CWD=%CD%
+set FOAMVER=None
+
+if %FOAMVER% GEQ 1000 goto OPENCFD
+
+:FOUNDATION
+set OLDPATH=%PATH%
+call %FOAMDIR%\setvars_OF%FOAMVER%.bat
+set PATH=%PATH%;%OLDPATH%
+
+REM Fix for error in FOAM_MPI in setvars-OF.bat
+FOR /F "tokens=* USEBACKQ" %%F IN (`dir /b %%FOAM_LIBBIN%%\MS-MPI*`) DO (
+SET FOAM_MPI=%%F
+)
+set PATH=%FOAM_LIBBIN%\%FOAM_MPI%;%PATH%
+goto CONTINUE
+
+:OPENCFD
+call %FOAMDIR%\setEnvVariables-v%FOAMVER%.bat
+
+:CONTINUE
+
+cd /d %CWD%
+
+REM Run PowerShell script
+PowerShell -NoProfile -ExecutionPolicy Bypass -File Allmesh.ps1
