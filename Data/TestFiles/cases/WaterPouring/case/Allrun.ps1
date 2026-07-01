b47b994685c0fd14c77a753b81909d0cdb032192
muezabdalla <muezabdalla777@gmail.com>
2026-07-01 15:10:11 +0300

    update testfiles for waterPouring demo (the use of the new setfileds dict)

--- Data/TestFiles/cases/WaterPouring/case/Allrun.ps1
+++ Data/TestFiles/cases/WaterPouring/case/Allrun.ps1
@@ -84,7 +84,15 @@ else
 }
 
 # Set internal fields according to setFieldsDict
-runCommand setFields
+# use the OF13 dict for setFields if using openfoam 13 and later
+if ( $Env:WM_PROJECT_VERSION[0] -ne "v" -and 13 -le $Env:WM_PROJECT_VERSION )
+{
+	runCommand setFields -dict system/setFieldsDict_OF13
+}
+else
+{
+	runCommand setFields
+}
 
 $PNAME = "p_rgh"
 
